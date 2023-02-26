b519e7a0e1e6a3b6b45d0569cfd3a19f0c8473c0
placiak <gerlach.tatras@gmail.com>
2023-02-26 02:55:09 +0000

    Completed Flight Tracker Mini Project

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,12 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+
+        //this.flightDAO.insertFlight(flight);
+
+
+
+        return this.flightDAO.insertFlight(flight);
     }
 
     /**
@@ -70,7 +75,24 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+
+
+        Flight flightNew = flightDAO.getFlightById(flight_id);
+
+        if(flightNew == null){
+
+            return null;
+        }else{
+
+            this.flightDAO.updateFlight(flight_id, flight);
+            return flightNew;
+        }
+
+
+    
+        
+
+        
     }
 
     /**
@@ -80,7 +102,7 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        return this.flightDAO.getAllFlights();
     }
 
     /**
@@ -92,6 +114,6 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        return this.flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
